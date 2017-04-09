d786d87150d72395476f68a770b92ea4ce2ef0c2
say2neeraj <say2neeraj@gmail.com>
2017-04-09 19:24:18 +0530

    First Commit/Push from ubuntu 16.04

---
+++ LetsStart/Basics/1PrintHelloString.py
@@ -0,0 +1,10 @@
+import sys
+import time
+
+print "Hello" #Used in Python 2.7.x versions
+print(sys.version)
+time.sleep(5)
+
+print ("Hello inside parenthesis ()")  #Used in Python 3.x versions
+print(sys.version)
+time.sleep(5)
